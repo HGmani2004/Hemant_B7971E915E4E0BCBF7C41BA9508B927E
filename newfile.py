a9a86501a5709c7431740cad91f0839d7ccc1c10
HGmani2004 <144554387+HGmani2004@users.noreply.github.com>
2023-10-18 23:52:30 +0530

    Add files via upload

--- newfile.py
+++ newfile.py
@@ -1,10 +1,25 @@
-def fact_rec(n):
-  if n == 0 or n == 1:
-    return 1
-  else:
-    return n * fact_rec(n - 1)
-
-
-number = 5
-res = fact_rec(number)
-print(" the factorial of {} is {}".format(number, res))
+class BankAccount:
+  def __init__(self, account_number, account_holder_name, initial_balance=0.0):
+      self.__account_number = account_number
+      self.__account_holder_name = account_holder_name
+      self.__account_balance = initial_balance
+  def deposit(self, amount):
+   if amount > 0:
+    self.__account_balance += amount
+    print("Deposited ₹{}. New balance: ₹{}".format(amount, self.__account_balance))
+   else:
+     print("Invalid deposit amount.please deposit a positive amount.")
+  def withdraw(self, amount):
+   if amount > 0 and amount <= self.__account_balance:
+     self.__account_balance -= amount
+     print("withdraw ₹{}. New balance: ₹{}".format(amount, self.__account_balance))
+   else:
+     print("Invalid withdrawl amount or insufficient balance.")
+  def display_balance(self):
+     print("Account balance for {} (account #{}): ₹{}".format(self.__account_holder_name, self.__account_number, self.__account_balance))
+account = BankAccount(account_number="123456789", account_holder_name="Hari Prabu", initial_balance=5000.0)
+account.display_balance()
+account.deposit(500.0)
+account.withdraw(200.0)
+account.withdraw(20000.0)
+account.display_balance()
